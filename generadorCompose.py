abde1782a77e05c2cd73293487aac2268f185c10
Nahuel Gomez <nngomez@fi.uba.ar>
2024-08-28 18:13:15 -0300

    completed volume creation with the config

--- generadorCompose.py
+++ generadorCompose.py
@@ -21,6 +21,8 @@ services:
     environment:
       - PYTHONUNBUFFERED=1
       - LOGGING_LEVEL=DEBUG
+    volumes:
+      - ./server/config.ini:/config.ini
     networks:
       - testing_net
 {clients}
@@ -44,6 +46,8 @@ for i in range(1, cantidad_clientes + 1):
     environment:
       - CLI_ID={i}
       - CLI_LOG_LEVEL=DEBUG
+    volumes:
+      - ./client/config.yaml:/config.yaml
     networks:
       - testing_net
     depends_on:
